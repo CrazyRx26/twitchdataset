73846613d9d1176872e4f1e13abf7b68efd5b1f8
Ryckson Oliveira <101434645+CrazyRx26@users.noreply.github.com>
2023-12-15 19:22:54 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import streamlit as st
 import pandas as pd
 
+import streamlit as st
+import pandas as pd
+
 # Carregando o conjunto de dados da Twitch
 df = pd.read_csv("twitchdata-update.csv")
 
 # Adicione uma coluna 'Contagem' ao DataFrame para armazenar a contagem de streamers
 df['Contagem'] = 1
+
+# Adicionando 44 linhas à tabela
+novas_linhas = 44
+for i in range(novas_linhas):
+    nova_linha = {"ColunaExistente": f"Dado{i+1}"}
+    df = df.append(nova_linha, ignore_index=True)
 
 st.title("Dataset - Twitch")
 
@@ -16,6 +25,6 @@ st.write(f"Saudações {nome}!")
 st.subheader("Quantidade de streamers:")
 st.write(f"Total de streamers: {len(df)}")
 
-# Display DataFrame or its columns
+# Exibindo informações do DataFrame
 st.subheader("Informações do DataFrame:")
-st.write(df.head())  # or st.write(df.columns)
+st.write(df.head())
