af8b17b8ea73bf9660eeb975259178d68f0955f4
Ryckson Oliveira <101434645+CrazyRx26@users.noreply.github.com>
2023-12-15 19:46:47 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 import streamlit as st
 import pandas as pd
+from pandas_profiling import ProfileReport
+import altair as alt
+import matplotlib.pyplot as plt
+import plotly.express as px
+import matplotlib
 
-# Carregando o conjunto de dados da Twitch
+# Carregando o conjunto de dados do Titanic
 df = pd.read_csv("twitchdata-update.csv")
 
-# Adicione uma coluna 'Contagem' ao DataFrame para armazenar a contagem de streamers
-df['Contagem'] = 1
+# Fonte dos Dados
+st.write("## Fonte dos Dados")
+st.write("Os dados da Twitch foram obtidos pelo Kaggle, no seguinte link:")
+st.write("Streamers - Twitch (https://www.kaggle.com/datasets/aayushmishra1512/twitchdata)")
 
-st.title("Dataset - Twitch")
+# Visão Geral
+st.write("## Visão Geral")
+st.write("Os jogos são uma indústria muito grande agora. Todos os anos, milhões de dólares são investidos em esportes eletrônicos e muitas novas empresas querem investir no cenário de esportes eletrônicos agora. Um dos maiores negócios de todos os tempos foi quando o Mixer abriu e trouxe Ninja e Shroud para sua plataforma a partir do Twitch. Mas o Twitch tem sido o lar de streamers desde o primeiro dia e agora que o Mixer foi encerrado, os streamers estão retornando à plataforma novamente. Milhões, senão bilhões, assistem streams do Twitch todos os dias.")
+st.write("Esses dados consistem em informações diferentes, como número de espectadores, número de espectadores ativos, seguidores ganhos e muitas outras colunas relevantes sobre um determinado streamer. Possui 11 colunas diferentes com todas as informações necessárias.")
 
-nome = st.text_input("Informe seu nome")
-st.write(f"Saudações {nome}!")
+# Estrutura do Conjunto de Dados
+st.write("## Estrutura do Conjunto de Dados")
+st.write(f"- **Número de Linhas (Amostras):** {df.shape[0]}")
+st.write(f"- **Número de Colunas (Variáveis):** {df.shape[1]}")
 
-# Informação da quantidade de streamers
-st.subheader("Quantidade de streamers:")
-st.write(f"Total de streamers: {len(df)}")
-
-# Display DataFrame or its columns
-st.subheader("Informações do DataFrame:")
-st.write(df.head())  # or st.write(df.columns)
+# Colunas Principais
+st.write("## Colunas Principais")
+for col in df.columns:
+    st.write(f"{col}: {df[col].nunique()} valores únicos")
